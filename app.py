725a422628433f1b244a6e18abd93c08120e7101
heetkoradia <heetkoradia@gmail.com>
2023-07-28 09:55:24 +0000

    revert shuffle

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 from question_generator import generate_question_word, generate_question_number, question_3, question_4, question_5
 
-import random
-
 app = Flask(__name__)
 CORS(app)  # Add this line to enable CORS for the entire app
 
@@ -14,8 +12,6 @@ question_templates = [
     (question_4,),
     (question_5,)
 ]
-
-random.shuffle(question_templates)
 
 @app.route('/questions', methods=['GET'])
 def get_questions():
